7253af902974ac697e40f6f55e21bc1d42598495
ART0022VI <artem.jidov@gmail.com>
2023-10-29 11:42:26 +0700

    upd

--- flask_pac3/app.py
+++ flask_pac3/app.py
@@ -209,7 +209,7 @@ def upload():
     if warnhuman > 0 or warngun > 0:
         result = "!ВНИМАНИЕ! Возможна угроза безопасности"
     else: 
-        result = "Угроза не обнаружена"
+        result = "Угроза не обнаружена, но не теряйте бдительность и проверяйте изображения на счёт красных рамок"
     #process_and_save_predictions(test_path, model, device, output_path, threshold=0.7) - не нужна 
     return render_template('./upload.html', result=result, image_path=image_path)
 
